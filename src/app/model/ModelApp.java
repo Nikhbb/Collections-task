5e6835555ef3b670586f64e6dfd30928c8537b8d
vnovikov <nik.hbb@gmail.com>
2024-03-07 00:37:57 +0200

    Refactoring model app

--- src/app/model/ModelApp.java
+++ src/app/model/ModelApp.java
@@ -2,18 +2,18 @@ package app.model;
 
 import app.entity.User;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ModelApp {
 
     public List<User> initList() {
-        List<User> users = new ArrayList<>();
-        users.add(0, new User("Ann", "ann@gmail.com"));
-        users.add(1, new User("Ivan", "ivan@ukr.net"));
-        users.add(2, new User("Jon", "jon@i.ua"));
-        users.add(3, new User("Tom", "tom@gmail.com"));
-        users.add(4, new User("Fox", "fox@gmail.com"));
-        return users;
+        return Arrays.asList(
+                new User("Ann", "ann@gmail.com"),
+                new User("Ivan", "ivan@ukr.net"),
+                new User("Jon", "jon@i.ua"),
+                new User("Tom", "tom@gmail.com"),
+                new User("Fox", "fox@gmail.com")
+        );
     }
 }
